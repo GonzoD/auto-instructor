f514c002fe4a96c72f2f27665c8706dfb4bbbf28
GonzoD <igor.k@4irelabs.com>
2021-09-02 22:05:30 +0300

    fix server on hosting

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 const cors = require('cors')
 const router = require('./routes/index')
 
@@ -11,6 +12,14 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
+
+// для разработки можно удалить, 
+//заменить ссылку env на клиенте - localhost:3000
+//и использовать команду npm run dev
+app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
 
 const start = async () => {
     try {
